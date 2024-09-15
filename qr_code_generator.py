a609a843f13fec83db8d7dacfbeb039eb4d8da2e
chanaka subasinghage <chanaka.s@icloud.com>
2024-09-15 13:31:00 +0200

    add strip for user inputs in the qr_generator code

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,7 +1,7 @@
 import qrcode
 
-user_input = input("Enter the text or URL: ")
-file_name = input("Enter the filename: ")
+user_input = input("Enter the text or URL: ").strip()
+file_name = input("Enter the filename: ").strip()
 
 img = qrcode.make(user_input)
 img.save(file_name)
