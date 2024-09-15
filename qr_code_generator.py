b1d377e0372d84275bf187335dd2e17aef21289c
chanaka subasinghage <chanaka.s@icloud.com>
2024-09-15 13:36:23 +0200

    modify qr code generator file

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,7 +1,16 @@
 import qrcode
 
-user_input = input("Enter the text or URL: ").strip()
+user_data = input("Enter the text or URL: ").strip()
 file_name = input("Enter the filename: ").strip()
 
-img = qrcode.make(user_input)
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(user_data)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
 img.save(file_name)
+print(f'QR code saved as {file_name}')
